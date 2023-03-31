a04ae5d4db36126f1dff5412ba9677be6b026419
Stevenz02 <jssmorales51@gmail.com>
2023-03-30 21:08:09 -0500

    2 Commit: Se agrego el constructor y el main

--- ejercicios/src/Comandosgithub/Persona.java
+++ ejercicios/src/Comandosgithub/Persona.java
@@ -4,5 +4,15 @@ public class Persona {
 	
 	private String nombre;
 	private String correo;
+	private String hola;
+	
+	public Persona (String nombre, String correo, String hola) {
+		this.nombre = nombre;
+		this.correo = correo;
+		this.hola = hola;
+	}
+	public static void main(String[] args) {
+		
+	}
 
 }
